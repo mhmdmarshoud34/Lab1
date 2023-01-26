4daa3733be205e0e0a819f1a51d1566dc7393b96
Mohammed Marshoud <mhmdmarshoud34@gmail.com>
2023-01-26 20:29:56 +0200

    initial info provided

---
+++ Profile.java
@@ -0,0 +1,7 @@
+public class Profile {
+    public static void main(String[] args) {
+        System.out.println("My name is Mohammed Marshoud");
+        System.out.println("My major is Computer Engineering");
+        System.out.println("Year of study: First Year");
+    }
+}
