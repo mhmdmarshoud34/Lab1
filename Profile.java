6d0a84ca79d632126e53db271c15a407bedbb9b6
Mohammed Marshoud <mhmdmarshoud34@gmail.com>
2023-01-26 20:42:25 +0200

    Revert "My hobby is added"
    This reverts commit 45e2eea46a68882dc75933d092b9d44514b69c92.

--- Profile.java
+++ Profile.java
@@ -3,6 +3,5 @@ public class Profile {
         System.out.println("My name is Mohammed Marshoud");
         System.out.println("My major is Computer Engineering");
         System.out.println("Year of study: First Year");
-	  System.out.println("My hobby is to play football");
     }
 }
