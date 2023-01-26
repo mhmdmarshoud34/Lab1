dd9d74f6a75cdcd10c13f77c17146454f49e8081
Mohammed Marshoud <mhmdmarshoud34@gmail.com>
2023-01-26 20:24:48 +0200

    This is the correction of the Problems

---
+++ Problems.java
@@ -0,0 +1,11 @@
+public class Problems{
+	public static void main(String[] args){
+		System.out.print("Hi, can we check this?");
+		System.out.println("It has a lot of problems");
+		System.out.print("Yeah, I know !!!");
+    		//this a comment;
+	}
+
+
+
+}
