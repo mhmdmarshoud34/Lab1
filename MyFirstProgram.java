3c806751c2eb3aec9a4eb57cb4fafee3354ff2fe
Mohammed Marshoud <mhmdmarshoud34@gmail.com>
2023-01-26 19:20:01 +0200

    My First Program is done

---
+++ MyFirstProgram.java
@@ -0,0 +1,13 @@
+public class MyFirstProgram{
+	public static void main(String[] args){
+		
+		System.out.print("This is my");
+		System.out.print("first Java Program");
+		System.out.print("Programming is fun!");
+
+
+	}
+
+
+
+}
